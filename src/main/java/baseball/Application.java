98798066252011da13ae4639ce96728220f20647
k-kmw <100478309+k-kmw@users.noreply.github.com>
2023-12-04 09:58:17 +0900

    Revert "[review] 야구게임 김민욱"

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,15 +1,7 @@
 package baseball;
 
-import baseball.controller.BaseballGameController;
-import baseball.view.InputView;
-import baseball.view.OutputView;
-
 public class Application {
     public static void main(String[] args) {
-
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        BaseballGameController baseballGameController = new BaseballGameController(inputView, outputView);
-        baseballGameController.start();
+        // TODO: 프로그램 구현
     }
 }
